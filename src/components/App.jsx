844ca18b5f54d4de8038245659292b5b2186616b
Vivek7d <vivekdhalkari07@gmail.com>
2023-12-19 23:41:11 +0530

    fourth change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,26 @@
 import React,{useState} from "react";
 
 function App(){
-    const[item,setItem]= useState({
-        task:""
-    })
+    const[item,setItem]= useState("")
     const[list,setList]=useState([])
     function handleChange(event){
-        const {value,name}=event.target
-        setItem(prevValue=>{
-            return{
-                ...prevValue.task,
-                [name]:value
-                
-            }
-        });
+        const newValue=event.target.value
+        setItem(newValue);
     }
     function handleClick(event){
-        setList(prevList=>[...prevList,item.task]);
-        setItem({task:""})
+        setList(prevList=>[...prevList,item]);
+        setItem("")
         event.preventDefault();
     }
     function handleDelete(index){
-        setList(prevList=>prevList.filter((_, i) => i !== index))
+        setList(prevList=>prevList.filter((_, i) => i !== index));
     }
     return(
         <div className="container">
             <div className="card">
                 <h1>To-Do List</h1>
                 <form>
-                    <input onChange={handleChange} type="text" name="task"/>
+                    <input onChange={handleChange} type="text" value={item} />
                     <button onClick={handleClick}>Add +</button>
                 </form>
                 <div className="listContainer">
