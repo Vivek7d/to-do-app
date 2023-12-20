74df398fa43c71ec5223a1e946cd1a8826e78b31
Vivek7d <vivekdhalkari07@gmail.com>
2023-12-20 23:06:20 +0530

    last-1 change

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToDoItem(props){
-    return(
-        <div>
-            <li key={props.id}>{props.task}
-            <button className="btn" onClick={() => props.onChecked(props.id) }> X </button>
-            </li>
-        </div>
-    );
+function ToDoItem(props) {
+  const [line, setLine] = useState(false);
+  function handleLine() {
+    setLine((prevValue) => {
+      return !prevValue;
+    });
+  }
+  return (
+    <div>
+      <li
+        onClick={handleLine}
+        style={{ textDecoration: line ? "line-through" : "none" }}
+        key={props.id}
+      >
+        {props.task}
+        <button className="btn" onClick={() => props.onChecked(props.id)}>
+          {" "}
+          X{" "}
+        </button>
+      </li>
+    </div>
+  );
 }
 export default ToDoItem;
